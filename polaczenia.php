dbd144c37a977bb9b57261e7d7e4c581dc84bbb9
bartekmarciniak <bartekmarciniak1993@gmail.com>
2018-01-14 20:40:41 +0100

    wersja 1

---
+++ polaczenia.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbhost="localhost"; $dbuser="barmarcp_bartek"; $dbpassword="23101993"; $dbname="barmarcp_bartek";
+$polaczenie = mysqli_connect ($dbhost, $dbuser, $dbpassword);
+mysqli_select_db ($polaczenie, $dbname);
+
+
+?>
