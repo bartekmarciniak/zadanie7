667aa7f250566ebb268db7c503cc8fca20f9607e
bartekmarciniak <bartekmarciniak1993@gmail.com>
2018-01-14 22:32:35 +0100

    wersja 1

---
+++ dodajpliki.php
@@ -0,0 +1,15 @@
+
+<html>
+ <body>
+<?php
+
+
+
+ <form action="odbierz.php" method="POST"
+ ENCTYPE="multipart/form-data">
+ <input type="hidden" name="rej" value="rej">
+ <input type="file" name="plik"/>
+ <input type="submit" value="Wyślij plik"/>
+ </form>
+ </body>
+</html>
