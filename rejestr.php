aa5450b1cf4d6cc425b9c631597f2c42947a2c41
bartekmarciniak <bartekmarciniak1993@gmail.com>
2018-01-14 21:17:01 +0100

    wersja 3

--- rejestr.php
+++ rejestr.php
@@ -21,29 +21,6 @@ $czas = date("Y-m-d H:i:s");
             $login='';
 			$haslo='';
 
-<?php
-ob_start();
-?>
-<html>
-<head>
-	  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
-</head>
-<body>
-
-
-<?php
-
-
-include ("polaczenia.php");
-$czas1 = time ();
-$czas2 = date ("r", $czas1);
-$czas = date("Y-m-d H:i:s");
-
-			@$rej = trim($_REQUEST["rej"]);
-            $login='';
-			$haslo='';
-
 
 			if ($rej == "rej")
 			{
@@ -57,13 +34,13 @@ $czas = date("Y-m-d H:i:s");
 				$blad = '0';
 				if (empty($login) == true)
 				{
-					$blad_01 = "<font color='black'><i></i>Musisz wypełnić pole login</font></br>";
+					$blad_01 = "<font color='black'><i></i>Musisz wypełnić pole login!!!</font></br>";
 					$blad='1';
 				}
 
 				if (empty($haslo) == true)
 				{
-					$blad_02 = "<font color='black'><i></i>Musisz wypełnić pole hasło</font></br>";
+					$blad_02 = "<font color='black'><i></i>Musisz wypełnić pole hasło!!!</font></br>";
 					$blad='1';
 				}
 				
@@ -80,9 +57,9 @@ $czas = date("Y-m-d H:i:s");
 				{
 					$wynik = mysqli_query ($polaczenie, "INSERT INTO user (login,haslo) VALUES ('". $login."','".$haslo."')");
 					//$zapytanie1= mysqli_query ($polaczenie, "INSERT INTO logi2 (idu,data,proby) VALUES ('". $id."','".$czas."','1')");
-					
+					if(mkdir('/home/barmarcp/domains/barmarc.pl/public_html/zad7/'.$login, 0777))
 					{
-					echo 'Udało się stworzyć katalogu';
+					//echo 'Udało się stworzyć katalog';
 					}
 					else
 					{
@@ -99,64 +76,6 @@ $czas = date("Y-m-d H:i:s");
 					}
 				
 				}
-			}
-ob_end_flush();		
-?>
-
-
-<center>
-<form action="" method="post">
-<input type="hidden" name="rej" value="rej">
-
-<p class='tytul'>ZARELESTRUJ SIĘ!!!</p>
-
-<p class='tytul'>Wpisz dane użytkownika</p><br/>
-Podaj login:</br><input type="text" name="login" value="<?php echo $login; ?>"></br></br>
-
-Podaj hasło:</br><input type="password" name="haslo" ></br></br>
-
-
-<input type="submit" name="B3" value="Zarejestruj się!"></br></br></br></br>
-</form>
-
-</center>
-
-</body>
-</html>
-
-
-			if ($rej == "rej")
-			{
-				$login = trim($_POST["login"]);
-				$haslo = trim($_POST["haslo"]);
-
-		
-				$blad_01='';
-				$blad_02='';
-				
-				$blad = '0';
-				if (empty($login) == true)
-				{
-					$blad_01 = "<font color='black'><i></i>Musisz wypełnić pole login</font></br>";
-					$blad='1';
-				}
-
-				if (empty($haslo) == true)
-				{
-					$blad_02 = "<font color='black'><i></i>Musisz wypełnić pole hasło</font></br>";
-					$blad='1';
-				}
-				
-				if ($blad =='1')
-				{
-					echo "<table border='1px' BGCOLOR='white'><tr><td align='center'>";
-					echo $blad_01;
-					echo $blad_02;
-
-					echo "</td></tr></table>";					
-				}
-				
-				
 			}
 ob_end_flush();		
 ?>
